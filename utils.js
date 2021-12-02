c822af1fcce6413e2d8092d8236d7612406b2eaa
CameronHStewart <cameronhstewart@gmail.com>
2021-12-02 06:48:00 +0000

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,7 @@
+function get_local_storage() {
+    let aaa_storage = localStorage.getItem('aaa_storage');
+    let json_aaa_storage = {};
+    if(aaa_storage != null) {
+        json_aaa_storage = JSON.parse(aaa_storage);
+    }
+}
