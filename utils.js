80cf6a44e5f3726515a0c4c272549c71d4046dc8
CameronHStewart <cameronhstewart@gmail.com>
2021-12-02 07:41:33 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,12 +1,7 @@
 console.log('Log at load!!!');
 
-window.bot_utils = {
+Window.bot_utils = {
     load_demo: () => {
         console.log('Import worked 2!!!');   
     }
 }
-
-function log_demo() {
-    console.log('Import worked!!!');   
-}
- 
