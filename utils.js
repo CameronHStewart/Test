cfde57d8dc6c61c495dc9be051eb791d68b67529
CameronHStewart <cameronhstewart@gmail.com>
2021-12-02 07:08:57 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,13 @@
 console.log('Log at load!!!');
+
+function get_local_storage() {
+    let aaa_storage = localStorage.getItem('aaa_storage');
+    let json_aaa_storage = {};
+    if(aaa_storage != null) {
+        json_aaa_storage = JSON.parse(aaa_storage);
+    }
+}
+
+function log_demo() {
+    console.log('Import worked!!!');   
+}
